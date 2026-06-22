c90f26501645ad5b95db6c37f92c25dfcef4c0a8
isahlopesss <isadorafl05@gmail.com>
2026-06-22 16:05:25 -0400

    fix: removendo arquivo "morto"

--- lib/gsap-setup.ts
+++
@@ -1,8 +0,0 @@
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-export const registerGSAP = () => {
-    if (typeof window !== 'undefined') {
-        gsap.registerPlugin(ScrollTrigger);
-    }
-};
